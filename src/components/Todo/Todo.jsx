15ea397ca2636872e74eda78bc6b39e67bf01a6d
Oleq <oleq.korzhit@gmail.com>
2024-09-29 18:24:38 +0300

    todo list is completed relise 1

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,20 +1,90 @@
 import TodoItem from "./TodoItem.jsx";
 import Title from "@/components/Title/Title.jsx";
 import '@/styles/components/todo.css'
+import {useEffect, useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function Todo() {
+    const [value, setValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    function inputHandler(e) {
+        setValue(e.target.value);
+    }
+
+    function buttonHandler() {
+        if(value.trim() === "") {
+            return;
+        }
+
+        setTodos((prev) => {
+            return [...prev, {
+                id: uuidv4(),
+                completed: false,
+                text: value,
+            }]
+        })
+
+        setValue("");
+    }
+
+    function deleteTask(id) {
+        const isConfirm = window.confirm("Are you sure you want to delete this task?");
+
+        if(isConfirm) {
+            setTodos(todos => todos.filter(el => el.id !== id));
+        }
+    }
+
+    function toggleTask(id) {
+        setTodos(todos => todos.map(task => {
+            if(task.id === id) {
+                return {
+                    ...task, completed: !task.completed
+                }
+            }
+
+            return task;
+        }));
+    }
+
+    function renderTodos(todos) {
+        return todos.map(task => {
+           return <TodoItem key={task.id} {...task} deleteTask={deleteTask} toggleTask={toggleTask} />
+        });
+    }
+
+    useEffect(() => {
+        try {
+            const storedTodos = JSON.parse(localStorage.getItem('todos'));
+
+            if(storedTodos) {
+                setTodos(storedTodos);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
     return (
         <div className="todo">
             <Title className="todo__title" text="Todo" />
             <div className="todo__head">
-                <input className="todo__input" type="text"/>
-                <button className="todo__button">Add Todo</button>
+                <input className="todo__input" type="text" value={value} onChange={inputHandler}/>
+                <button className="todo__button" onClick={buttonHandler}>Add Todo</button>
             </div>
             <div className="todo__inner">
-                <span className="todo__empty">На даний момент список порожній</span>
-                <div className="todo__content">
-
-                </div>
+                {todos.length === 0 && <span className="todo__empty">На даний момент список порожній</span>}
+                {todos.length > 0 && (
+                    <div className="todo__content">
+                        {renderTodos(todos)}
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,6 +1,19 @@
-function TodoItem() {
+import '@/styles/components/todoItem.css';
+import deleteIcon from "@/assets/icons/delete.svg";
+
+function TodoItem({id, text, completed, deleteTask, toggleTask}) {
     return (
-        <></>
+        <div className={`todo-item ${completed ? 'todo-item--completed' : ''}`}>
+            <div className="todo-item__content">
+                <label className="todo-item__label" htmlFor={`check-${id}`}>
+                    <input className="todo-item__checkbox" id={`check-${id}`} type="checkbox" checked={completed} onChange={() => toggleTask(id)}/>
+                    <span className="todo-item__text">{text}</span>
+                </label>
+            </div>
+            <div className="todo-item__delete" onClick={() => deleteTask(id)}>
+                <img className="todo-item__delete-icon" src={deleteIcon} alt="delete"/>
+            </div>
+        </div>
     );
 }
 
